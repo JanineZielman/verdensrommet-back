c0f2a08b364c7b3ae585d4856188f68ffc8af630
root <root@h2956023.stratoserver.net>
2022-05-19 21:11:16 +0200

    update

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1337), 
+  url: 'https://cms.verdensrommet.network',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '21c3b81867c02e3856233a4c948aa60e'),
